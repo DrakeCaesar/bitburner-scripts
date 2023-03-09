a8bc1913715a7120b903491395abcc885eb75758
Dominik Cisár <dominik.cisar@gmail.com>
2023-03-10 00:35:38 +0100

    simplified crawl

--- src/crawl.ts
+++ src/crawl.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { NS } from "@ns"
 
 function crawl(
@@ -15,65 +14,10 @@ function crawl(
    })
 }
 
-function crawlFast(
-   ns: NS,
-   knownServers: Set<string>,
-   startNode = ns.getHostname()
-): void {
-   const queue = [startNode]
-   const visited = new Set()
-
-   while (queue.length > 0) {
-      const currentNode = queue.shift()!
-
-      if (!knownServers.has(currentNode)) {
-         knownServers.add(currentNode)
-      }
-
-      visited.add(currentNode)
-
-      const neighbors = ns.scan(currentNode)
-      for (const neighbor of neighbors) {
-         if (!visited.has(neighbor)) {
-            queue.push(neighbor)
-            visited.add(neighbor)
-         }
-      }
-   }
-}
-
-export function crawlOld(
-   ns: NS,
-   knownServersOld: string[],
-   hostname = "",
-   depth = 0
-) {
-   const servers = ns.scan(hostname)
-   for (const element of servers) {
-      if (!knownServersOld.includes(element)) {
-         knownServersOld.push(element)
-         crawlOld(ns, knownServersOld, element, depth + 1)
-      }
-   }
-}
-
 export async function main(ns: NS): Promise<void> {
    const knownServers = new Set<string>()
    crawl(ns, knownServers)
    const sortedServers = Array.from(knownServers).sort()
    ns.tprint(`servers: ${sortedServers.length}`)
    ns.tprint(sortedServers)
-
-   knownServers.clear()
-
-   crawlFast(ns, knownServers)
-   const sortedServersFast: string[] = Array.from(knownServers).sort()
-   ns.tprint(`servers: ${sortedServersFast.length}`)
-   ns.tprint(sortedServersFast)
-
-   const knownServersOld: string[] = []
-   crawlOld(ns, knownServersOld)
-   const sortedServersOld = knownServersOld.sort()
-   ns.tprint("servers: " + sortedServersOld.length)
-   ns.tprint(sortedServersOld)
 }
