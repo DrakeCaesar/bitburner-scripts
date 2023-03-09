d420e06b143ef8d118b99d2aa0d3b0da5d7912e3
Dominik Cisár <dominik.cisar@gmail.com>
2023-03-10 00:35:57 +0100

    made workout easier to read

--- src/workout.ts
+++ src/workout.ts
@@ -1,25 +1,20 @@
-import { NS } from ".."
+import { NS } from "@ns"
 
 export async function main(ns: NS): Promise<void> {
    for (;;) {
       const focus = ns.singularity.isFocused()
-      const skills = ns.getPlayer().skills
-      if (
-         skills.agility < skills.dexterity &&
-         skills.agility < skills.defense &&
-         skills.agility < skills.strength
-      ) {
-         ns.singularity.gymWorkout("powerhouse gym", "Agility", focus)
-      } else if (
-         skills.dexterity < skills.defense &&
-         skills.dexterity < skills.strength
-      ) {
-         ns.singularity.gymWorkout("powerhouse gym", "Dexterity", focus)
-      } else if (skills.defense < skills.strength) {
-         ns.singularity.gymWorkout("powerhouse gym", "Defense", focus)
-      } else {
-         ns.singularity.gymWorkout("powerhouse gym", "Strength", focus)
-      }
-      await ns.sleep(1000)
+      const exp = ns.getPlayer().exp
+      const skillOrder = [
+         { name: "Agility", value: exp.agility },
+         { name: "Dexterity", value: exp.dexterity },
+         { name: "Defense", value: exp.defense },
+         { name: "Strength", value: exp.strength },
+      ]
+      ns.getPlayer().skills
+      const minSkill = skillOrder.reduce((prev, curr) =>
+         prev.value < curr.value ? prev : curr
+      )
+      ns.singularity.gymWorkout("powerhouse gym", minSkill.name, focus)
+      await ns.sleep(250)
    }
 }
