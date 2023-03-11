2cd5f7619be9968ad8c56e00c12c2994a9d494eb
Dominik Cisár <dominik.cisar@gmail.com>
2023-03-11 22:54:57 +0100

    made glow white and scale with luminance

--- src/glow.ts
+++ src/glow.ts
@@ -4,13 +4,23 @@ export async function main(ns: NS): Promise<void> {
    // Define the CSS class for the glow effect
    const glowClass = "glow"
 
-   // Define the styles for the glow effect
-   const glowStyles = `
-    text-shadow: 0 0 5px rgba(255, 255, 255, 0.5);
-`
+   // Function to calculate the luminance value of a given color
+   function calculateLuminance(color: string): number {
+      const rgb = color.substring(4, color.length - 1).split(",")
+      const r = parseInt(rgb[0].trim(), 10) / 255
+      const g = parseInt(rgb[1].trim(), 10) / 255
+      const b = parseInt(rgb[2].trim(), 10) / 255
+      return 0.2126 * r + 0.7152 * g + 0.0722 * b
+   }
 
    // Function to apply the glow effect to a given text element
    function applyGlowEffectToElement(element: HTMLElement) {
+      const color = getComputedStyle(element).color
+      const luminance = calculateLuminance(color)
+      const intensity = 0.1 + luminance * 0.9
+      const glowStyles = `
+        text-shadow: 0 0 ${intensity * 10}px rgba(255, 255, 255, ${intensity});
+    `
       element.classList.add(glowClass)
       element.style.cssText += glowStyles
    }
