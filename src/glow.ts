b074794b95083f577a20ea63b9567459bd50f6c7
Dominik Cisár <dominik.cisar@gmail.com>
2023-03-12 01:02:12 +0100

    made both text and svgs glow

--- src/glow.ts
+++ src/glow.ts
@@ -14,20 +14,46 @@ export async function main(ns: NS): Promise<void> {
    }
 
    // Function to apply the glow effect to a given text element
-   function applyGlowEffectToElement(element: HTMLElement) {
+   function applyGlowEffectToTextElement(element: HTMLElement) {
       const color = getComputedStyle(element).color
       const luminance = calculateLuminance(color)
       const intensity = 0.1 + luminance * 0.9
       const glowStyles = `
-        text-shadow: 0 0 ${
-           intensity * 10
-        }px rgba(255, 255, 255, ${intensity}) !important;
-    `
+            text-shadow: 0 0 ${
+               intensity * 10
+            }px rgba(255, 255, 255, ${intensity}) !important;
+        `
       element.classList.add(glowClass)
       element.style.cssText += glowStyles
    }
 
-   // Function to apply the glow effect to all text elements in a given element
+   // Function to apply the glow effect to an SVG element
+   function applyGlowEffectToSvgElement(element: SVGElement) {
+      const color = getComputedStyle(element).fill
+      const luminance = calculateLuminance(color)
+      const opacity = luminance * 0.6
+      const filter = document.createElementNS(
+         "http://www.w3.org/2000/svg",
+         "filter"
+      )
+      filter.setAttribute("id", "glow-filter")
+      const feDropShadow = document.createElementNS(
+         "http://www.w3.org/2000/svg",
+         "feDropShadow"
+      )
+      feDropShadow.setAttribute("dx", "0")
+      feDropShadow.setAttribute("dy", "0")
+      feDropShadow.setAttribute("stdDeviation", "5")
+      feDropShadow.setAttribute("flood-color", "white")
+      feDropShadow.setAttribute("flood-opacity", String(opacity))
+      filter.appendChild(feDropShadow)
+      element.insertBefore(filter, element.firstChild)
+      element.style.filter = "url(#glow-filter)"
+      element.style.margin = "-10px"
+      element.style.padding = "10px"
+   }
+
+   // Function to apply the glow effect to all text and SVG elements in a given element
    function applyGlowEffectToElementsInContainer(container: HTMLElement) {
       const textNodes = document.createTreeWalker(
          container,
@@ -52,14 +78,25 @@ export async function main(ns: NS): Promise<void> {
 
       let currentNode: Node | null
       while ((currentNode = textNodes.nextNode())) {
-         applyGlowEffectToElement(currentNode.parentElement as HTMLElement)
+         if (currentNode.parentElement instanceof SVGElement) {
+            applyGlowEffectToSvgElement(currentNode.parentElement)
+         } else {
+            applyGlowEffectToTextElement(
+               currentNode.parentElement as HTMLElement
+            )
+         }
+      }
+
+      const svgElements = container.getElementsByTagName("svg")
+      for (const svgElement of svgElements) {
+         applyGlowEffectToSvgElement(svgElement)
       }
    }
 
-   // Apply the glow effect to all text elements on page load
+   // Apply the glow effect to all text and SVG elements on page load
    applyGlowEffectToElementsInContainer(document.body)
 
-   // Set up a mutation observer to apply the effect to new text elements
+   // Set up a mutation observer to apply the effect to new text and SVG elements
    const observer = new MutationObserver((mutationsList) => {
       for (const mutation of mutationsList) {
          if (mutation.type === "childList") {
@@ -67,6 +104,8 @@ export async function main(ns: NS): Promise<void> {
             addedElements.forEach((element) => {
                if (element instanceof HTMLElement) {
                   applyGlowEffectToElementsInContainer(element)
+               } else if (element instanceof SVGSVGElement) {
+                  applyGlowEffectToSvgElement(element)
                }
             })
          }
