d82e925951d376b7963a0ffc47a457d81bbc4346
Dominik Cisár <dominik.cisar@gmail.com>
2025-09-21 21:19:52 +0200

    added pnpm workspace

--- src/dependencyTest/testModA.ts
+++ src/dependencyTest/testModA.ts
@@ -1,5 +1,18 @@
 import { NS } from "@ns"
 
 export function testa(ns: NS) {
+  const playery = ns.getPlayer()
+
   ns.bladeburner.getActionCurrentLevel("", "")
+
+  ns.bladeburner.getActionCurrentLevel("", "")
+
+  const playerx = ns.getPlayer()
+  ns.sleep(1000)
+
+  ns.bladeburner.getActionCurrentLevel("", "")
+  ns.sleep(1000)
+
+  const player = ns.getPlayer()
+  const moneyMax = ns.getServerMaxMoney("test")
 }
