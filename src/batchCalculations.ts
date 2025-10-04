af6fff5f85d785f228199b4a81dfdf62b0014bdc
Dominik Cisár <dominik.cisar@gmail.com>
2025-10-04 14:01:29 +0200

    added a missing script

--- src/batchCalculations.ts
+++ src/batchCalculations.ts
@@ -108,6 +108,7 @@ export async function copyRequiredScripts(ns: NS, host: string) {
   ns.scp("/prepareServer.js", host)
   ns.scp("/batchCalculations.js", host)
   ns.scp("/batchVisualizerStub.js", host)
+  ns.scp("/libraries/crawl.js", host)
 }
 
 export async function prepareServer(ns: NS, host: string, target: string) {
